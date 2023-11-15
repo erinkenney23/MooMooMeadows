b9f74aeceb2c407e7795e93ac9f73157be871396
erinkenney23 <erinkenney23@gmail.com>
2023-11-15 11:39:58 -0500

    Initial Commit

---
+++ MooMooMeadows/MooMooMeadowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooMooMeadowsApp.swift
+//  MooMooMeadows
+//
+//  Created by Erin Kenney on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MooMooMeadowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
